69a5cf8e592098168aff0b44b59da086d4aacd6e
skawlgns <skawlgns@kakao.com>
2024-04-20 14:35:12 +0900

    useEffect

---
+++ src/components/Products.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+
+export default function Products() {
+  const [count, setCount] = useState(0);
+  const [products, setProducts] = useState([]);
+  const [checked, setChecked] = useState(false);
+
+  const handleChnage = () => {
+    setChecked((prev) => !prev);
+  };
+
+  useEffect(() => {
+    fetch(`data/${checked ? 'sale_' : ''}products.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('🔥뜨끈한 데이터를 네트워크에서 받아옴');
+        setProducts(data);
+      });
+    return () => {
+      console.log('🧹 깨끗하게 청소하는 일들을 합니다.');
+    };
+  }, [checked]);
+
+  return (
+    <>
+      <input
+        type="checkbox"
+        value={checked}
+        id="checkbox"
+        onClick={handleChnage}
+      />
+      <label htmlFor="checkbox">Show sale list</label>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <article>
+              <h3>{product.name}</h3>
+              <p>{product.price}</p>
+            </article>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
